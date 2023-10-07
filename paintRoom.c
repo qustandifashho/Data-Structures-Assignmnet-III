ab822c08542d8fb8f880eeb8e3f1fe736e29362e
qustandifashho <75642117+qustandifashho@users.noreply.github.com>
2023-10-06 20:17:03 -0500

    Update paintRoom.c

--- paintRoom.c
+++ paintRoom.c
@@ -36,14 +36,12 @@ void paintRoom( RoomData room ){
             if (room.roomArray[i][j] == 'A'){
                 startingRow = i;
                 startingColumn = j;
-                printf("Found 'A' at Row: %d, Column: %d\n", startingRow, startingColumn); // remove before submission 
+                //printf("Found 'A' at Row: %d, Column: %d\n", startingRow, startingColumn); // remove before submission 
 
                 break;
             }
         }
-        // if(startingRow == -1){
-        //     break; // getting out of outer for loop if A is found. 
-        // }
+       
     
     }
      if (startingRow == -1 || startingColumn == -1){
@@ -57,52 +55,38 @@ void paintRoom( RoomData room ){
 }
 
  
-
+// check if there is letter in that place 
 /* TODO
  * recPaintRoom
  * input: the room to process, the row and column of the current location being explored, the distance traveled from 'A'
  * output: N/A
  */
 void recPaintRoom(RoomData room, int row, int col, int distanceFromA /* feel free to remove/add any other parameters here*/ ) {
-    char currentDistance = room.roomArray[row][col] - 'A';
+    char currentDistance = room.roomArray[row][col] - 'A'; // calculates the distance from the character in the roomArray at the specified row and col coordinates to the character 'A'
 
-    // printf("TEST 1 (void recPointRoom)\n"); // should print 
     // Base case: Check if the current location is outside the room boundaries or is an obstacle/wall.
     if (row < 0 || col < 0 || row >= room.numrows || col >= room.numcols || room.roomArray[row][col] == '*') { // basically if not in the room boundaries
         return;
     }
-    //printf("TEST 2 (void recPointroom)\n"); 
     
     // Base case: Check if the current location has been visited already AND IF IT HITS A WALL OR OBSTACLE FIRST 
-    /////// || A AND SEE WHAT ELSE BECAUASE THIS IS WHERE A BIG PROBLEM IS ~ FIXED but A does not show up
     if ((room.roomArray[row][col] != ' ') && (room.roomArray[row][col] != 'A')) {
         // If it has been visited before, check if the new distance is smaller, and update if necessary.
-        //char currentDistance = room.roomArray[row][col] - 'A';
+        //char currentDistance = room.roomArray[row][col] - 'A';(put it at the top of the functions)
         if (distanceFromA < currentDistance) {
+            recPaintRoom(room, row - 1, col, distanceFromA + 1); // Up
+            recPaintRoom(room, row, col + 1, distanceFromA + 1); // Right
+            recPaintRoom(room, row + 1, col, distanceFromA + 1); // Down
+            recPaintRoom(room, row, col - 1, distanceFromA + 1); // Left
             room.roomArray[row][col] = 'A' + distanceFromA;
             
         }
         return;
     }
-    //printf("TEST 3 (void recPointroom)\n");
-  
-    //printf("TEST 4 (void recPointroom)\n");
-    // Mark the current location as visited with the letters 'A' to 'Z'
-    // if (distanceFromA <= 25) {
-    //     room.roomArray[row][col] = 'A' + distanceFromA;
-    //     //printf("%d", distanceFromA);
-    // } 
-    // /*else if(distanceFromA == 0){
-    //     room.roomArray[row][col] = 'A' + distanceFromA ;
-    // }*/
-    // else {
-    //     room.roomArray[row][col] = 'Z'; // If distance is 25 or more, use 'Z' (would only be 26 or higher because dan havbe more than 26 steps which would just keep giving z with each step).
-    // }
-
 
 
     // Mark the current location as visited with the letters 'A' to 'Z', except if it's already 'A'.
-    if (room.roomArray[row][col] != 'A') {
+    if ((room.roomArray[row][col] != 'A') && (room.roomArray[row][col] == ' ') ) {
         if (distanceFromA <= 25) {
         room.roomArray[row][col] = 'A' + distanceFromA;
     } 
@@ -111,16 +95,16 @@ void recPaintRoom(RoomData room, int row, int col, int distanceFromA /* feel fre
     }
 }
 
-
-   // printf("TEST 5 (void recPointroom)\n");
     // Recursive calls to go to the next space.
     recPaintRoom(room, row - 1, col, distanceFromA + 1); // Up
     recPaintRoom(room, row, col + 1, distanceFromA + 1); // Right
     recPaintRoom(room, row + 1, col, distanceFromA + 1); // Down
     recPaintRoom(room, row, col - 1, distanceFromA + 1); // Left
-    //printf("TEST 6 (void recursion)\n");
+
 
 }
+
+// everytime you update you have you to go into the recursion again
 
 
 
